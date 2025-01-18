4790f831dcb59988d712d4a025574ee93bd7767c
xuebi <myxuebi@outlook.com>
2025-01-18 12:35:11 +0800

    修改一些语法问题（乱ctrl-v导致的

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
-    ARCH="amd64"; 
-elif [ "$ARCH" = "aarch64" ]; then 
-    ARCH="arm64";
-else 
-    echo "Unsupported architecture"; 
-    exit 1; 
-fi 
+    ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+else
+    echo "Unsupported architecture"
+    exit 1
+fi
 cd /app/agent
 /app/agent/agent-linux-$ARCH &
 cd /app/dashboard
