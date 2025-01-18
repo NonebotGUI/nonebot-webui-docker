443c524fbb630eb8c3a2c19429851f7f9adaeaf7
xuebi <myxuebi@outlook.com>
2025-01-18 12:30:56 +0800

    修改的启动脚本忘记push了

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"; 
+elif [ "$ARCH" = "aarch64" ]; then 
+    ARCH="arm64";
+else 
+    echo "Unsupported architecture"; 
+    exit 1; 
+fi 
 cd /app/agent
-/app/agent/agent-linux &
+/app/agent/agent-linux-$ARCH &
 cd /app/dashboard
-/app/dashboard/dashboard-linux
+/app/dashboard/dashboard-linux-$ARCH
