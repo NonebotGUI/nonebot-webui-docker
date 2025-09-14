45611202539e78c00c6f2e4e959860b1046a8a92
myxuebi <myxuebi@outlook.com>
2025-09-14 21:03:50 +0800

    修复每次启动密码会自动修改的问题

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 if [ -z "$(ls -A /data)" ]; then
     cp -r /app/* /data/
+    TOKEN=$(head -c 32 /dev/urandom | base64 | tr -dc 'A-Za-z0-9' | head -c 32)
+    sed -i "s/\"token\": \".*\"/\"token\": \"$TOKEN\"/g" /data/agent/config.json
+    sed -i "s/\"token\": \".*\"/\"token\": \"$TOKEN\"/g" /data/dashboard/config.json
+    sed -i "s/\"password\": \".*\"/\"password\": \"$TOKEN\"/g" /data/dashboard/config.json
 fi
-TOKEN=$(head -c 32 /dev/urandom | base64 | tr -dc 'A-Za-z0-9' | head -c 32)
-sed -i "s/\"token\": \".*\"/\"token\": \"$TOKEN\"/g" /data/agent/config.json
-sed -i "s/\"token\": \".*\"/\"token\": \"$TOKEN\"/g" /data/dashboard/config.json
-sed -i "s/\"password\": \".*\"/\"password\": \"$TOKEN\"/g" /data/dashboard/config.json
 sh /data/entrypoint.sh
